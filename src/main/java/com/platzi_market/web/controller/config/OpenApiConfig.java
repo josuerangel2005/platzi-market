e3399145015c4625a739686cb84d26569c7a544c
Yenderson Josué Rangel Martínez <josuerangel262005@gmail.com>
2025-12-28 19:13:36 -0500

    Finalizado

--- src/main/java/com/platzi_market/web/controller/config/OpenApiConfig.java
+++ src/main/java/com/platzi_market/web/controller/config/OpenApiConfig.java
@@ -1,3 +1,5 @@
+package com.platzi_market.web.controller.config;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
